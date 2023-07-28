f61bf4c2040dd379eee37f7e5a02a74cab5a37fa
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-07-28 17:10:07 +0300

    Added resize grid button and added trail effect when hovering over grid.

--- index.js
+++ index.js
@@ -3,20 +3,54 @@ const mainGridContainer = document.querySelector('.main-grid-container');
 function createGrid(size) {
     for (i = 0; i < size; i++) {
         const row = document.createElement('div');
-        row.classList.add('row'+(i+1));
+        row.classList.add('row');
         row.style.display = 'flex';
         row.style.justifyContent = 'cemter';
         row.style.alignItems = 'center';
         mainGridContainer.appendChild(row);
         for (j = 0; j < size; j++) {
             const gridSquare = document.createElement('div');
-            gridSquare.style.width = '48px';
-            gridSquare.style.height = '48px';
+            gridSquare.style.width = (mainGridContainer.clientWidth / size) + 'px';
+            gridSquare.style.height = (mainGridContainer.clientHeight / size) + 'px';
             gridSquare.style.border = '1px Solid Black';
+            gridSquare.style.boxSizing = 'border-box';
             gridSquare.style.backgroundColor = 'white';
             row.appendChild(gridSquare);
         }
     }
+    hover();
 }
+
+function hover() {
+    const rows = document.querySelectorAll('.row');
+
+    for (i = 0; i < rows.length; i++) {
+        let squares = rows[i].children;
+        for (j = 0; j < squares.length; j++) {
+            squares[j].addEventListener('mouseenter', (e) => {
+                e.target.style.backgroundColor = 'black';
+            });
+
+            squares[j].addEventListener('mouseleave', (e) => {
+                e.target.style.backgroundColor = 'white';
+            });
+        }
+    }
+}
+
+
+function clearGrid() {
+    const rows = document.querySelectorAll('.row');
+    for (i = 0; i < rows.length; i++) {
+        rows[i].remove();
+    }
+}
+
+const resizeButton = document.querySelector('#resize-grid-button');
+resizeButton.addEventListener('click', () => {
+    let userInput = prompt('Enter a new size for the grid: ');
+    clearGrid();
+    createGrid(userInput);
+});
 
 createGrid(16);
